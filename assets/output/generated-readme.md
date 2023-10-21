f47cd4db9515d2bea60bc308afa6afdc3b8143ba
nestibry <nestibry@gmail.com>
2023-10-21 10:40:39 -0500

    started and tested a simple init() function to write to file

---
+++ assets/output/generated-readme.md
@@ -0,0 +1,1 @@
+{"title":"Bryan's Project"}
--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the Project Title?",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer.prompt(questions)
+    .then(response => {
+        console.log(response);
+        var responseStr = JSON.stringify(response);
+        fs.writeFile('./assets/output/generated-readme.md', responseStr, (err) => err ? console.log(err) : console.log('Successfuly generated file!'));
+    });
+
+}
 
 // Function call to initialize app
 init();
